39e4934ea511cfa97aec6707159856b6e38687ab
Sidharth <ahujasidharth24@gmail.com>
2024-09-03 21:46:13 +0530

    fix auctions closing

--- pages/api/auctions/latest-auction.ts
+++ pages/api/auctions/latest-auction.ts
@@ -28,62 +28,61 @@ async function closePreviousAuction(previousAuctionId: string) {
   }
 
   if (allFishInAuction?.length && allFishInAuction.length > 0) {
-    for (let i = 0; i < allFishInAuction.length; i++) {
-      // @ts-ignore
-      const upperLimitOfGenePool = allFishInAuction[i].fishtypes.total;
-      const randFishIndex = Math.floor(Math.random() * upperLimitOfGenePool);
-      const { data: allFishBids, error: allFishBidsError } = await supabase
-        .from('bids')
-        .select('user_id, bid_amount')
-        .eq('fish_id', allFishInAuction[i].id)
-        .eq('auction_id', previousAuctionId)
-        .order('bid_amount', { ascending: false });
-
-      if (allFishBidsError) {
-        console.error(
-          `Error fetching all fish(${allFishInAuction[i].id}) bids  in auction`,
-          allFishBidsError
-        );
-        throw allFishBidsError;
-      }
-
-      console.log('ALL FISH BIDS:', allFishBids);
-      if (allFishBids?.length && allFishBids.length > 0) {
-        const winner = allFishBids[0].user_id;
+    // run updates concurrently using promise
+    await Promise.all(
+      allFishInAuction.map(async (fish) => {
         // @ts-ignore
-        const fishTypeId = allFishInAuction[i].fishtypes.id;
-        const newFilePath = `metadata/${winner}/${allFishInAuction[i].id}/metadata.json`;
+        const upperLimitOfGenePool = fish.fishtypes.total;
+        const randFishIndex = Math.floor(Math.random() * upperLimitOfGenePool);
 
-        const { data: copiedData, error: copyDataError } =
-          await supabase.storage
+        const { data: allFishBids, error: allFishBidsError } = await supabase
+          .from('bids')
+          .select('user_id, bid_amount')
+          .eq('fish_id', fish.id)
+          .eq('auction_id', previousAuctionId)
+          .order('bid_amount', { ascending: false });
+
+        if (allFishBidsError) {
+          console.error(`Error fetching all fish(${fish.id}) bids  in auction`, allFishBidsError);
+          throw allFishBidsError;
+        }
+
+        console.log('ALL FISH BIDS:', allFishBids);
+
+        if (allFishBids?.length && allFishBids.length > 0) {
+          const winner = allFishBids[0].user_id;
+          // @ts-ignore
+          const fishTypeId = fish.fishtypes.id;
+          const newFilePath = `metadata/${winner}/${fish.id}/metadata.json`;
+
+          const { error: copyDataError } = await supabase.storage
             .from('gene_pool')
             .copy(`${fishTypeId}/${randFishIndex}/metadata.json`, newFilePath);
 
-        if (copyDataError) {
-          if (copyDataError.message != 'The resource already exists') {
+          if (copyDataError && copyDataError.message !== 'The resource already exists') {
             console.error(`Error copying metadata`, copyDataError);
             throw copyDataError;
           }
+
+          const { error: updateFishError } = await supabase
+            .from('fish')
+            .update({
+              state: 'revealed',
+              owner_address: winner,
+              winning_bid: allFishBids[0].bid_amount,
+              metadata_url: `${process.env.NEXT_PUBLIC_SUPABASE_BASE_URL}${newFilePath}`,
+            })
+            .eq('id', fish.id);
+
+          if (updateFishError) {
+            console.error(`Error updating fish`, updateFishError);
+            throw updateFishError;
+          }
+
+          console.log('FISH AWARED TO:', winner, newFilePath);
         }
-
-        const { data, error: updateFishError } = await supabase
-          .from('fish')
-          .update({
-            state: 'revealed',
-            owner_address: winner,
-            winning_bid: allFishBids[0].bid_amount,
-            metadata_url: `${process.env.NEXT_PUBLIC_SUPABASE_BASE_URL}${newFilePath}`,
-          })
-          .eq('id', allFishInAuction[i].id);
-
-        console.log('FISH AWARED TO:', winner, newFilePath);
-
-        if (updateFishError) {
-          console.error(`Error updating fish`, updateFishError);
-          throw updateFishError;
-        }
-      }
-    }
+      })
+    );
   }
 }
 
@@ -92,11 +91,9 @@ async function createNewAuction(batchNumber: number) {
   const NEW_AUCTION_DESCRIPTION = 'This is a new auction';
   const AUCTION_START_PRICE = '1.00';
   const BATCH_PREFIX = generateRandomString(2);
-  // Create a new auction with the same details
+
   const newStartTime = new Date();
   const newEndTime = new Date(newStartTime);
-
-  // newEndTime.setDate(newEndTime.getDate() + 1);
   newEndTime.setMinutes(newEndTime.getMinutes() + 5);
 
   const { data: newAuctionData, error: newAuctionError } = await supabase
@@ -133,19 +130,17 @@ async function createNewAuction(batchNumber: number) {
     // Choose randomly between 10 to 20 fish in each auction batch
     const randumNumOfFish = Math.floor(Math.random() * (20 - 10) + 10);
 
-    let newAuctionFishList = [];
-    for (let i = 0; i < randumNumOfFish; i++) {
-      // Choose between 0 and the upper limit of the index
-      const randFishTypeIndex = Math.floor(
-        Math.random() * fishTypesList.length
-      );
-      const fishType = fishTypesList[randFishTypeIndex];
-      newAuctionFishList.push({
-        fishtype_id: fishType.id,
-        auction_id: newAuctionId,
-        serial_number: BATCH_PREFIX + '-' + (i + 1),
-      });
-    }
+    const newAuctionFishList = Array.from({ length: randumNumOfFish }).map(
+      (_, i) => {
+        const randFishTypeIndex = Math.floor(Math.random() * fishTypesList.length);
+        const fishType = fishTypesList[randFishTypeIndex];
+        return {
+          fishtype_id: fishType.id,
+          auction_id: newAuctionId,
+          serial_number: BATCH_PREFIX + '-' + (i + 1),
+        };
+      }
+    );
 
     const { error: inserFishError } = await supabase
       .from('fish')
@@ -170,7 +165,7 @@ async function createNewAuction(batchNumber: number) {
 }
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  let newAuctionId: null = null;
+  let newAuctionId: string | null = null;
 
   if (req.method === 'GET') {
     try {
@@ -203,8 +198,20 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       const now = new Date();
       const lastAuctionEndTime = new Date(latestAuction.end_time);
 
-      if (now > lastAuctionEndTime) {
+      // Check if the last auction is already closed
+      if (latestAuction.state === 'closed') {
+        console.log('Latest auction is already closed, creating a new one.');
+        try {
+          newAuctionId = await createNewAuction(latestAuction.batch_number + 1);
+        } catch (error: any) {
+          return res.status(500).json({
+            error: `Error creating new auction: ${error.message}`,
+          });
+        }
+      } else if (now > lastAuctionEndTime) {
+        console.log('Auction has ended, closing the previous auction.');
         await closePreviousAuction(latestAuction.id);
+
         // Update the state of the previous auction to 'closed'
         const { error: updateError } = await supabase
           .from('auctions')
@@ -215,11 +222,16 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
           return res.status(500).json({
             error: `Error updating previous auction state: ${updateError.message}`,
           });
+        } else {
+          console.log('Previous auction closed successfully.');
         }
 
         try {
+          console.log('Creating a new auction...');
           newAuctionId = await createNewAuction(latestAuction.batch_number + 1);
+          console.log('New auction created with ID:', newAuctionId);
         } catch (error: any) {
+          console.error('Error creating new auction:', error);
           return res.status(500).json({
             error: `Error creating new auction: ${error.message}`,
           });
