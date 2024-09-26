fe9e008b170dcd63447205066e13c0b6cb8198ac
Manish Choudhary <manishchoudhary9663@gmail.com>
2024-09-26 11:27:41 +0530

    feat: to store invitation in supabase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,17 @@
-import Head from 'next/head';
-import HomePageContent from '@/components/HomeTwo';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import Head from "next/head";
+import HomePageContent from "@/components/HomeTwo";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import useInvitationCode from "@/hooks/useInvitationCode";
 
 export default function Home() {
   const router = useRouter();
   const inviteCode = router?.query?.inviteCode;
+  const codeStatus = useInvitationCode();
 
   useEffect(() => {
-    if (inviteCode && typeof inviteCode === 'string') {
-      window.localStorage.setItem('invite_code', inviteCode);
+    if (inviteCode && typeof inviteCode === "string") {
+      window.localStorage.setItem("invite_code", inviteCode);
     }
   }, [inviteCode]);
 
@@ -17,9 +19,9 @@ export default function Home() {
     <>
       <Head>
         <title>HashTank</title>
-        <meta name='description' content='Save the last fish in the world' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="description" content="Save the last fish in the world" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <HomePageContent />
     </>
