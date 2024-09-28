0b863aae83941a51be255a7d9408b7380ac128de
Manish Choudhary <manishchoudhary9663@gmail.com>
2024-09-29 00:07:18 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,18 @@ export default function Home() {
 
   return (
     <>
+      {/* {codeStatus === null && <p>Checking invitation code...</p>}
+    {codeStatus != null && 
+    <div>
+      <Head>
+        <title>HashTank</title>
+        <meta name="description" content="Save the last fish in the world" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <HomePageContent />
+    </div>
+    } */}
       <Head>
         <title>HashTank</title>
         <meta name="description" content="Save the last fish in the world" />
